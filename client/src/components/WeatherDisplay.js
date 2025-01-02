59acd7a766b70d4a71b4800c65a7c8c1412bde36
SibhatY <sabto6659@gmail.com>
2025-01-01 18:40:12 -0600

    style - beautification of UI

--- client/src/components/WeatherDisplay.js
+++ client/src/components/WeatherDisplay.js
@@ -1,15 +1,28 @@
 import React from 'react';
+import './WeatherDisplay.css';
 
 function WeatherDisplay({ data }) {
     const { name, main, weather, wind } = data;
 
     return (
-        <div className="weather-display">
-            <h2>Weather in {name}</h2>
-            <p>Temperature: {main.temp}°</p>
-            <p>Condition: {weather[0].description}</p>
-            <p>Humidity: {main.humidity}%</p>
-            <p>Wind Speed: {wind.speed} mph</p>
+
+
+        <div className='weather-card'>
+            <h2>{name}</h2>
+            <div className='weather-info'>
+                <div className='weather-condition'>
+                    <img src={`http://openweathermap.org/img/wn/${weather[0].icon}@2x.png`} alt={weather[0].description} />
+
+                    <p>{weather[0].description}</p>
+                </div>
+                <div className='weather-stats'>
+
+                    <p><strong>Temperature:</strong> {main.temp}°</p>
+                    <p><strong>Condition:</strong> {weather[0].description}</p>
+                    <p><strong>Humidity:</strong> {main.humidity}%</p>
+                    <p><strong>Wind Speed:</strong> {wind.speed} mph</p>
+                </div>
+            </div>
         </div>
     );
 }
