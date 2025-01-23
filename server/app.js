0534084bfebf708de99e509e1b7e929d20616c9e
SibhatY <sabto6659@gmail.com>
2025-01-22 19:29:22 -0600

    feat: Add helmet  for security and express-rate-limit to set a max for api requests

--- server/app.js
+++ server/app.js
@@ -1,7 +1,20 @@
 const express = require('express');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
 const weatherRoutes = require('./routes/weatherRoutes');
 const app = express();
 
-app.use('/api/weather', weatherRoutes);
+app.use(helmet());
+app.use(express.static('public'));
+app.set('trust proxy', 1);
+
+const apiLimiter = rateLimit({
+
+    windowMs: 15*60*1000,
+    max: 100,
+    message: 'Too many requests, please try again after a short time.'
+});
+
+app.use('/api/weather', apiLimiter, weatherRoutes);
 
 module.exports = app;
